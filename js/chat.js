14fe91920f68e91ed0575da27a42c6f340f29d1c
ribo <roibeard.mac.unfraidh@gmail.com>
2019-07-14 18:19:38 +0200

    Add temporary results for selecting options A B and C in chat.js

--- js/chat.js
+++ js/chat.js
@@ -13,7 +13,23 @@ $(document).ready(function() {
       playerName = $('#ainm').val();
       $('.modal-content').html('');
       $('.modal-content').append(`<h1>fáilte ` + playerName + `!</h1>`);
-      //   setTimeout()
+      setTimeout(function() {
+        $('.modal').fadeOut('slow');
+      }, 2000);
     }
+  });
+
+  $('.button-b').click(function() {
+    $('.modal-content').html('');
+    $('.modal-content').append(
+      `<h1><span id="Geaga's monologue to go here">Cainnt Geaga le dul anseo</h1>`
+    );
+    setTimeout(function() {
+      $('.modal').fadeOut('slow');
+    }, 2000);
+  });
+
+  $('.button-c').click(function() {
+    $('.modal').fadeOut('slow');
   });
 });
